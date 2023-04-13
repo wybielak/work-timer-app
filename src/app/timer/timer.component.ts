7001a25693d4e3f234a70e7425ed735c838bb73b
wybielak <31769799+wybielak@users.noreply.github.com>
2023-04-14 00:20:05 +0200

    Timer Ready

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -8,11 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class TimerComponent implements OnInit {
 
   start_time = new Date();
+  start_hours: any;
+  start_minutes: any;
+
   end_time = new Date();
-  time = 0;
+  end_hours: any;
+  end_minutes: any;
+
+  time = 0
 
   hours = 0;
   minutes = 0;
+  seconds = 0;
+
+  intv: any;
 
   is_on = false;
 
@@ -20,24 +29,35 @@ export class TimerComponent implements OnInit {
       
   }
 
+  timeInator() {
+    this.time += 1;
+    this.update_time();
+  }
+
   start_work() {
-    this.start_time = new Date()
-    this.time = 0
-    this.update_time()
-    this.is_on = true
+    this.start_time = new Date();
+    this.intv = setInterval(() => this.timeInator(), 1000);
+    
+    this.start_hours = this.start_time.getHours();
+    this.start_minutes = this.start_time.getMinutes();
+
+    this.time = 0;
+    this.is_on = true;
   }
 
   end_work() {
-    this.end_time = new Date()
-    this.time = (this.end_time?.getTime() - this.start_time.getTime()) / 1000;
-    this.update_time()
+    this.end_time = new Date();
+    clearInterval(this.intv);
+
+    this.end_hours = this.end_time.getHours();
+    this.end_minutes = this.end_time.getMinutes();
+
     this.is_on = false;
   }
-
+  
   update_time() {
-    this.minutes = Math.round(Math.floor(this.time % 3600) / 60);
+    this.seconds = Math.floor(this.time % 60);
+    this.minutes = Math.floor((this.time / 60) % 60);
     this.hours = Math.floor(this.time / 3600);
   }
-
-
 }
